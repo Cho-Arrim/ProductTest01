a23bf5fe7cddd6cc8622d050246a482416ed5e29
Cho-Arrim <daniel.linder@bybrick.se>
2023-02-24 14:51:26 +0100

    TEsting Babylon

--- product01/src/components/HelloWorld.vue
+++ product01/src/components/HelloWorld.vue
@@ -1,60 +1,94 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <main>
+  <div id="loader">
+    <p>Loading</p>
+    <div id="loadingContainer">
+      <div id="loadingBar"></div>
+    </div>
+    <p id="percentLoaded">25%</p>
   </div>
+  <p>Custom Loading Screen</p>
+  <canvas>
+
+  </canvas>
+  </main>
 </template>
 
 <script lang="ts">
+import { CameraMechanics } from '@/scripts/CameraMechanics';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
+  mounted(){
+    const canvas = document.querySelector("canvas")!;
+    const loadingBar = document.getElementById('loadingBar') as HTMLElement;
+    const percentLoaded = document.getElementById('percentLoaded') as HTMLElement;
+    const loader = document.getElementById('loader') as HTMLElement;
+    //new MeshActions(canvas, loadingBar, percentLoaded, loader);
+    new CameraMechanics(canvas, loadingBar, percentLoaded, loader);
+  }
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto:wght@100;700&display=swap");
+main {
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#loader {
+  width: 100%;
+  height: 100%;
+  background: slategrey;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#loaded {
+  width: 100%;
+  height: 100%;
+  background: slategrey;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+  transition: opacity 1s ease;
 }
-a {
-  color: #42b983;
+#loadingContainer {
+  width: 30%;
+  height: 2rem;
+  background: rgba(255, 255, 255, 0.75);
+  border-radius: 6px;
+  margin: 0.5rem;
+}
+#loadingBar {
+  height: 100%;
+  background: green;
+  border-radius: 6px;
+}
+p {
+  color: white;
+  background: none;
+  margin-bottom: 1rem;
+  font-family: "Roboto Condensed";
+  font-weight: 400;
+  font-size: 2rem;
+}
+canvas {
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  box-shadow: 8px 8px 10px -6px #000000;
 }
 </style>
